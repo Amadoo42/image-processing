ccea253beab960237f736e0a97ed5654f54fc480
Seif-Lashin <seif.mlashin1911@gmail.com>
2025-09-22 20:33:04 +0300

    Create BlackandWhite.h
    This Filter changes image to Black&White format

---
+++ src/filters/BlackandWhite.h
@@ -0,0 +1,17 @@
+void BlackandWhite(Image &image){
+    //grayscale(image)
+    for(int i=0;i<image.width;i++){
+        for(int j=0;j<image.height;j++){
+            if(image(i,j,0)>=128){
+                image(i,j,0)=255;
+                image(i,j,1)=255;
+                image(i,j,2)=255;
+            }
+            else{
+                image(i,j,0)=0;
+                image(i,j,1)=0;
+                image(i,j,2)=0;
+            }
+        }
+    }
+}
