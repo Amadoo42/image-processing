d5af1ff889c9ea24788a9fc0cb599f65f9c95938
Amadoo42 <amadoo2006@gmail.com>
2025-09-23 15:38:38 +0300

    Added invert filter and created .gitignore file.

---
+++ src/filters/invert.h
@@ -0,0 +1,9 @@
+void invert(Image &image) {
+    for(int i = 0; i < image.width; ++i) {
+        for(int j = 0; j < image.height; ++j) {
+            image(i, j, 0) = 255 - image(i, j, 0);
+            image(i, j, 1) = 255 - image(i, j, 1);
+            image(i, j, 2) = 255 - image(i, j, 2);
+        }
+    }
+}
