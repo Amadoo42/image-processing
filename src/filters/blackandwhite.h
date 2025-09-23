2142c60d92f3bd562ac06c319afbe53584c42795
Seif-Lashin <seif.mlashin1911@gmail.com>
2025-09-23 23:01:33 +0300

    Update blackandwhite.h
    implmented grayscale in it and using average luminance as threshold

--- src/filters/blackandwhite.h
+++ src/filters/blackandwhite.h
@@ -1,10 +1,21 @@
-#include "gray.h"
 
 void blackandwhite(Image &image){
-    gray(image);
-    for(int i=0;i<image.width;i++){
-        for(int j=0;j<image.height;j++){
-            if(image(i,j,0)>=128){
+    int sum=0;
+    for(int i=0; i < image.width; i++){
+        for(int j=0; j < image.height; j++){
+            int gry = 0.2126 * image(i,j,0) + 0.7152 * image(i,j,1) + 0.0722 * image(i,j,2);
+
+            for(int c = 0; c < 3; c++)image(i,j,c) = gry;
+            
+            sum += gry;
+        }
+    }
+
+    int threshold = sum / (image.width * image.height);
+
+    for(int i=0; i < image.width; i++){
+        for(int j=0; j < image.height; j++){
+            if(image(i,j,0) >= threshold){
                 image(i,j,0)=255;
                 image(i,j,1)=255;
                 image(i,j,2)=255;
