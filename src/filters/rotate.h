d98927b74b25d001cb139a19f09cd167962946c1
Amadoo42 <amadoo2006@gmail.com>
2025-09-23 16:11:40 +0300

    Added rotate filter

---
+++ src/filters/rotate.h
@@ -0,0 +1,16 @@
+void rotate(Image &image, int degrees) {
+    int numRotations = degrees / 90;
+    for(int k = 0; k < numRotations; ++k) {
+        int newWidth = image.height;
+        int newHeight = image.width;
+        Image newImage(newWidth, newHeight);
+        for(int j = 0; j < newWidth; ++j) {
+            for(int i = 0; i < newHeight; ++i) {
+                newImage(newWidth - j - 1, i, 0) = image(i, j, 0);
+                newImage(newWidth - j - 1, i, 1) = image(i, j, 1);
+                newImage(newWidth - j - 1, i, 2) = image(i, j, 2);
+            }
+        }
+        image = newImage;
+    }
+}
