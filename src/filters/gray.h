c7606922c98b3a2352cae913c0b1112676c1e486
Seif-Lashin <seif.mlashin1911@gmail.com>
2025-09-23 23:02:03 +0300

    Update gray.h
    slightly improved grayscale

--- src/filters/gray.h
+++ src/filters/gray.h
@@ -1,10 +1,8 @@
-void gray(Image & a) {
-    for(int i = 0; i < a.width; i++) {
-        for(int j = 0; j < a.height; j++) {
-            unsigned int avg = 0;
-            for(int k = 0; k < 3; k++) avg += a(i, j, k);
-            avg /= 3;
-            for(int k = 0; k < 3; k++) a(i, j, k) = avg;
+void gray(Image & image) {
+    for(int i=0; i < image.width; i++){
+        for(int j=0; j < image.height; j++){
+            int gry = 0.2126 * image(i,j,0) + 0.7152 * image(i,j,1) + 0.0722 * image(i,j,2);
+            for(int c = 0; c < 3; c++)image(i,j,c) = gry;
         }
     }
 }
