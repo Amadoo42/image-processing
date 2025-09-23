ab4c6ba404f6d45ac95be68b8fe9730913e98495
Seif-Lashin <seif.mlashin1911@gmail.com>
2025-09-23 22:16:07 +0300

    Update resize.h
    the filter now does a better job in blending the colors, resulting in a clearer less pixlated image

--- src/filters/resize.h
+++ src/filters/resize.h
@@ -1,18 +1,32 @@
+double color(Image &image,int i, int j, int c){
+    double srci= i * image.width / newWidth;
+    double srcj= j * image.height / newHeight;
+            
+    int x1 = srci, y1 = srcj, x2 = min(image.width-1,x1+1), y2 = min(image.height-1,y1+1);
+
+    return (x2 - srci) * (y2 - srcj) / ((x2 - x1) * (y2 -y1)) * image(x1,y1,c)
+                    + (srci - x1) * (y2 - srcj) / ((x2 - x1) * (y2 -y1)) * image(x2,y1,c)
+                    + (x2 - srci) * (srcj - y1) / ((x2 - x1) * (y2 -y1)) * image(x1,y2,c)
+                    + (srci - x1) * (srcj - y1) / ((x2 - x1) * (y2 -y1)) * image(x2,y2,c);
+}
 void resize(Image &image,int newWidth, int newHeight){
     Image newImage(newWidth,newHeight);
-    for(int i=0;i<newWidth;i++){
-        for(int j=0;j<newHeight;j++){
-            int srci= i * image.width / newWidth;
-            int srcj= j * image.height / newHeight;
-            newImage(i,j,0)=image(srci,srcj,0);
-            newImage(i,j,1)=image(srci,srcj,1);
-            newImage(i,j,2)=image(srci,srcj,2);
+    for(int i=0; i < newWidth; i++){
+        for(int j=0; j < newHeight; j++){
+
+            double r = color(image, x1, y1, x2, y2, 0);
+            double g = color(image, x1, y1, x2, y2, 1); 
+            double b = color(image, x1, y1, x2, y2, 2);
+
+            newImage(i,j,0) = r;
+            newImage(i,j,1) = g;
+            newImage(i,j,2) = b;
         }
     }
-    image=newImage;
+    image = newImage;
 }
 void resize(Image &image,double ratioX, double ratioY){
-    int newWidth=image.width * ratioX;
-    int newHeight=image.height * ratioY;
-    resize(image,newWidth,newHeight);
+    int newWidth = image.width * ratioX;
+    int newHeight = image.height * ratioY;
+    resize(image, newWidth, newHeight);
 }
