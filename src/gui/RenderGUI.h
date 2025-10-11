cd80ed8bee165c30e5084273f5dee9ad88fff2fd
Amadoo42 <amadoo2006@gmail.com>
2025-10-11 15:06:10 +0300

    Update RenderGUI.h

--- src/gui/RenderGUI.h
+++ src/gui/RenderGUI.h
@@ -7,29 +7,21 @@ void renderGUI(ImageProcessor &processor) {
     ImGui::Begin("Image Processor", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
 
     if(ImGui::Button("Load Image")) {
-        // #TODO: Open a file dialog here
-        // For now we'll load a hardcoded image.
-        // processor.loadImage("assets/SampleImages/luffy.jpg");
-        // std::cout << "Image loaded." << std::endl;
-        bool stop = 0;
-        while(!stop) {
-        std::string selected = openFileDialog_Linux();
-        bool manual_popup = 0;
-        char manual_buf[1024] = "";
+        bool done = 0;
+        while(!done) {
+            std::string selected = openFileDialog_Linux();
+            bool manual_popup = 0;
+            char manual_buf[1024] = "";
 
             if(!selected.empty()) {
-                // todo : make sure the directory leads to an image
+                // #TODO: Make sure the directory leads to an image
                 // std::cout << "Selected : " << selected << '\n';
-            std::cout << "loaded Image Successfully\n";
-            processor.loadImage(selected);
-            stop = 1;
-        }else {
-            std::cout << "try again\n";
+                std::cout << "Image loaded successfully!\n";
+                processor.loadImage(selected);
+                done = 1;
+            }
+            else std::cout << "Try again\n";
         }
-    }
-        // if(manual_popup) {
-
-        // }
     }
 
     ImGui::SameLine();
