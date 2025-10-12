d7f7fb5bb967662324839359efa292355d2e70b4
Amadoo42 <amadoo2006@gmail.com>
2025-10-12 07:34:18 +0300

    Previous 3 commits have refactored the code to add a FilterParameter.h file,
    The load directory is still not working on windows

--- src/gui/FilterParameters.h
+++ src/gui/FilterParameters.h
@@ -1,35 +1,156 @@
 #pragma once
-
 #include "../core/ImageProcessor.h"
+#include "../filters/GrayscaleFilter.h"
+#include "../filters/InvertFilter.h"
+#include "../filters/BlurFilter.h"
+#include "../filters/BlackAndWhiteFilter.h"
+#include "../filters/CropFilter.h"
+#include "../filters/ResizeFilter.h"
+#include "../filters/DarkenFilter.h"
+#include "../filters/LightenFilter.h"
+#include "../filters/FrameFilter.h"
+#include "../filters/HorizontalFlipFilter.h"
+#include "../filters/VerticalFlipFilter.h"
+#include "../filters/MergeFilter.h"
+#include "../filters/RotateFilter.h"
+#include "../filters/OutlineFilter.h"
+#include "../filters/PurpleFilter.h"
+#include "../filters/InfraredFilter.h"
+#include "../filters/WaveFilter.h"
+#include "../filters/OilPaintingFilter.h"
+#include "../filters/ContrastFilter.h"
+#include "../filters/RetroFilter.h"
+#include "../filters/SaturationFilter.h"
+#include "../filters/SkewFilter.h"
+#include "../filters/VigentteFilter.h"
+#include "../filters/WarmthFilter.h"
 
 class FilterParameters {
 public:
-    FilterParameters(ImageProcessor &proc);
+    FilterParameters(ImageProcessor &proc) : processor(proc) {};
 
-    void applyGrayscale();
-    void applyInvert();
-    void applyBlur();
-    void applyBlackAndWhite();
-    void applyCrop();
-    void applyResize();
-    void applyDarken();
-    void applyLighten();
-    void applyFrame();
-    void applyHorizontalFlip();
-    void applyVerticalFlip();
-    void applyMerge();
-    void applyRotate();
-    void applyOutline();
-    void applyPurple();
-    void applyInfrared();
-    void applyWave();
-    void applyOilPainting();
-    void applyContrast();
-    void applyRetro();
-    void applySaturation();
-    void applySkew();
-    void applyVignette();
-    void applyWarmth();
+    void applyGrayscale() {
+        GrayscaleFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Grayscale Filter\n";
+    }
+    void applyInvert() {
+        InvertFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Invert Filter\n";
+    }
+    void applyBlur() {
+        BlurFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Blur Filter\n";
+    }
+    void applyBlackAndWhite() {
+        BlackAndWhiteFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Black and White Filter\n";
+    }
+    void applyCrop() {
+        CropFilter filter(50, 50, 200, 200); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Crop Filter\n";
+    }
+    void applyResize() {
+        ResizeFilter filter(400, 400); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Resize Filter\n";
+    }
+    void applyDarken() {
+        DarkenFilter filter; 
+        processor.applyFilter(filter);
+        std::cout << "Applied Darken Filter\n";
+    }
+    void applyLighten() {
+        LightenFilter filter; 
+        processor.applyFilter(filter);
+        std::cout << "Applied Lighten Filter\n";
+    }
+    void applyFrame() {
+        Image frame_image("../assets/fancy.png");
+        FrameFilter filter(frame_image);
+        processor.applyFilter(filter);
+        std::cout << "Applied Frame Filter\n";
+    }
+    void applyHorizontalFlip() {
+        HorizontalFlipFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Horizontal Flip Filter\n";
+    }
+    void applyVerticalFlip() {
+        VerticalFlipFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Vertical Flip Filter\n";
+    }
+    void applyMerge() {
+        Image merge_image("../assets/SampleImages/luffy.jpg");
+        MergeFilter filter(merge_image); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Merge Filter\n";
+    }
+    void applyRotate() {
+        RotateFilter filter(90); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Rotate Filter\n";
+    }
+    void applyOutline() {
+        OutlineFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Outline Filter\n";
+    }
+    void applyPurple() {
+        PurpleFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Purple Filter\n";
+    }
+    void applyInfrared() {
+        InfraredFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Infrared Filter\n";
+    }
+    void applyWave() {
+        WaveFilter filter; 
+        processor.applyFilter(filter);
+        std::cout << "Applied Wave Filter\n";
+    }
+    void applyOilPainting() {
+        OilPaintingFilter filter; 
+        processor.applyFilter(filter);
+        std::cout << "Applied Oil Painting Filter\n";
+    }
+    void applyContrast() {
+        ContrastFilter filter(1.5); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Contrast Filter\n";
+    }
+    void applyRetro() {
+        RetroFilter filter;
+        processor.applyFilter(filter);
+        std::cout << "Applied Retro Filter\n";
+    }
+    void applySaturation() {
+        SaturationFilter filter(1.5); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Saturation Filter\n";
+    }
+    void applySkew() {
+        SkewFilter filter(40); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Skew Filter\n";
+    }
+    void applyVignette() {
+        VigentteFilter filter(0.5); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Vignette Filter\n";
+    }
+    void applyWarmth() {
+        WarmthFilter filter(1.5); 
+        processor.applyFilter(filter);
+        std::cout << "Applied Warmth Filter\n";
+    }
 
 private:
     ImageProcessor &processor;    
