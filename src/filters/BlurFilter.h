6861831a36d270bbbffb6cd6b43ee6ab603fb846
Amadoo42 <amadoo2006@gmail.com>
2025-11-07 16:03:47 +0200

    Optimize BlurFilter with downsampling and upsampling
    Improved the performance of BlurFilter by first downsampling the image to 25% resolution, applying the blur, and then upsampling back to the original size. This approach significantly increases speed while maintaining acceptable accuracy. Added dependency on ResizeFilter.

--- src/filters/BlurFilter.h
+++ src/filters/BlurFilter.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Filter.h"
+#include "ResizeFilter.h"
 #include <algorithm>
 #include <vector>
 
@@ -31,9 +32,16 @@ public:
     // Sigma determines the spread or width of the Gaussian kernel, which weights neighboring pixels around a central pixel
 
     void apply(Image &image) override {
-        int W = image.width, H = image.height;
-        std::vector <double> kernel = generateGaussianKernel();
+        int ogW = image.width, ogH = image.height;
 
+        // We've changed this to make it a little faster, where we first downsample the image to 25% of the resolution
+        // Then blur it then upsample it again. This significantly improves speed while not losing too much accuracy.
+        Image downsampledImage = image;
+        ResizeFilter downsample(ogW / 4, ogH / 4);
+        downsample.apply(downsampledImage);
+        
+        int W = downsampledImage.width, H = downsampledImage.height;
+        std::vector <double> kernel = generateGaussianKernel();
         Image temp(W, H);
         int half = kernelSize / 2;
 
@@ -43,12 +51,14 @@ public:
                     double weightedSum = 0.0;
                     for(int k = -half; k <= half; ++k) {
                         int nx = std::clamp(x + k, 0, W - 1);
-                        weightedSum += image(nx, y, c) * kernel[k + half];
+                        weightedSum += downsampledImage(nx, y, c) * kernel[k + half];
                     }
                     temp(x, y, c) = round(weightedSum);
                 }
             }
         }
+
+        Image upsampledImage(W, H);
 
         for(int x = 0; x < W; ++x) {
             for(int y = 0; y < H; ++y) {
@@ -58,9 +68,14 @@ public:
                         int ny = std::clamp(y + k, 0, H - 1);
                         weightedSum += temp(x, ny, c) * kernel[k + half];
                     }
-                    image(x, y, c) = round(weightedSum);
+                    upsampledImage(x, y, c) = round(weightedSum);
                 }
             }
         }
+
+        ResizeFilter upsample(ogW, ogH);
+        upsample.apply(upsampledImage);
+
+        image = upsampledImage;
     }
 };
